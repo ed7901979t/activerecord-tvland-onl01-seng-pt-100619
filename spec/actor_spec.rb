9e38d9dcb57c9ab5afed23993d8f35dfc99233ef
clean-command-1851 <ed7901979t@gmail.com>
2020-02-12 23:20:40 +0000

    Done.

--- spec/actor_spec.rb
+++ spec/actor_spec.rb
@@ -2,8 +2,13 @@ require_relative 'spec_helper'
 
 describe "Actor" do
   let(:actor) {Actor.new}
+  #TODO: implement the tests as described in the it blocks,
+  #      and implement the class and migrations required to pass them
+
+  # HINTS: look at show_spec.rb and network_spec.rb and character_spec.rb for guidance
 
   it "has a first and last name" do
+    # TODO set up the basic data model for actor
     actor = Actor.create(:first_name => "Emilia", :last_name => "Clarke")
 
     expect(actor.first_name).to eq("Emilia")
@@ -11,6 +16,8 @@ describe "Actor" do
   end
 
   it "has associated characters in an array" do
+    # Hint: think about what migration you'll need to write so that an actor can have many characters.
+    # Where will the association foreign key go?
     emilia = Actor.new(:first_name => "Emilia", :last_name => "Clarke")
     khaleesi = Character.new(:name => "Khaleesi")
     khaleesi.actor = emilia
@@ -44,6 +51,8 @@ describe "Actor" do
   end
 
   it "can list its full name" do
+    # TODO create an instance method on actor called full_name to return first and
+    #last name together
     emilia = Actor.new(:first_name => "Emilia", :last_name => "Clarke")
     emilia.save
 
@@ -52,6 +61,9 @@ describe "Actor" do
   end
 
   it "can list all of its shows and characters" do
+    # TODO create a list_roles method
+    # TODO: build a method on actor that will return an array of
+    # strings in the form "#{character_name} - #{show_name}"
     emilia = Actor.new(:first_name => "Emilia", :last_name => "Clarke")
     khaleesi = Character.new(:name => "Khaleesi")
     khaleesi.actor = emilia
@@ -63,3 +75,4 @@ describe "Actor" do
     expect(emilia.list_roles).to include("Khaleesi - Game of Thrones")
   end
 end
+
